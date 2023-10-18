2639f93f41cee535911aadcf6437a55b31292eb6
Bruno Souza <brunoalksouza@gmail.com>
2023-10-18 20:17:50 -0300

    add cors

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const body_parser = require("body-parser");
 const userRouter = require("./routers/user.router");
 const eventRouter = require("./routers/event.router");
+const cors = require("cors");
 
 const app = express();
 
 app.use(body_parser.json());
+// Middleware que carrega as permissões do CORS
+// Access-Control-Allow-Origin: *
+app.use(cors({ origin: "*" }));
 
 app.use("/", userRouter);
 app.use("/", eventRouter);
